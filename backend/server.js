67e82101f75353b6adf5dfd72c334dbd72b80b4d
Jericho Condesa <jerichocondesa@gmail.com>
2026-03-01 19:10:00 +0630

    fixing the session cookies

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000
     }
   })
