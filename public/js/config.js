95edf4e283998886e3defcdebabdb361661cc800
Jericho Condesa <jerichocondesa@gmail.com>
2026-03-17 17:27:09 +0630

    ito pag dipa

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,4 @@
-const API_BASE = 'html-previewergithubapp-production.up.railway.app';   
+
+const API_BASE = "https://html-previewergithubapp-production.up.railway.app";
 
 console.log("✅ API_BASE is set to:", API_BASE);
