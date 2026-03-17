18d693ee48d9b829b5c3b4e00f1e0875dc83013c
Jericho Condesa <jerichocondesa@gmail.com>
2026-03-17 16:57:39 +0630

    new

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,3 @@
-const API_BASE = window.location.origin;
+const API_BASE = 'https://html-previewer-github-app.up.railway.app';   // ← Put your real Railway URL here
+
+console.log("✅ API_BASE is set to:", API_BASE);
