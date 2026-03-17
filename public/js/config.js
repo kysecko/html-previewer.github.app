8a32be6987181bd1ed615e5077a4dfe9368132db
Jericho Condesa <jerichocondesa@gmail.com>
2026-03-17 17:23:53 +0630

    new

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-const API_BASE = 'html-previewer-github-app.railway.internal';   
+const API_BASE = 'html-previewergithubapp-production.up.railway.app';   
 
 console.log("✅ API_BASE is set to:", API_BASE);
