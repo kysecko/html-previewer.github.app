80664f0abfe32255be0f9cbda60e277260afad56
Jericho Condesa <jerichocondesa@gmail.com>
2026-03-18 14:41:01 +0630

    new

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -6,7 +6,7 @@ console.log('PROJECTS ROUTES LOADED AND WORKING');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 router.get('/test', (req, res) => {
